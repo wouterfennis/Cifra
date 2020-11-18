1ed135d59811d50cfd204f32f7e008864a4ef598
Wouter Fennis <WouterF@infosupport.com>
2020-11-18 22:17:22 +0100

    More unit testing

--- Cifra.FileSystem.UnitTests/Mapping/ClassMappingTests.cs
+++ Cifra.FileSystem.UnitTests/Mapping/ClassMappingTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AutoFixture;
+using Cifra.Application.Models.Class;
+using Cifra.FileSystem.Mapping;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +12,48 @@ namespace Cifra.FileSystem.UnitTests.Mapping
     [TestClass]
     public class ClassMappingTests
     {
+        private Fixture _fixture;
 
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void MapToFileEntity_InputNull_ThrowsException()
+        {
+            Class input = null;
+
+            Action action = () => input.MapToFileEntity();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void MapToFileEntity_WithValidClass_MapsClassProperties()
+        {
+            Class input = _fixture.Create<Class>();
+
+            FileEntity.Class result = input.MapToFileEntity();
+
+            result.Id.Should().Be(input.Id);
+            result.Name.Should().Be(input.Name.Value);
+        }
+
+        [TestMethod]
+        public void MapToFileEntity_WithValidClass_MapsStudents()
+        {
+            Class input = _fixture.Create<Class>();
+
+            FileEntity.Class result = input.MapToFileEntity();
+
+            result.Students.Should().HaveCount(input.Students.Count);
+
+            foreach (var inputStudent in input.Students)
+            {
+                result.Students.Should().Contain(x => x.FullName == inputStudent.FullName.Value);
+            }
+        }
     }
 }
