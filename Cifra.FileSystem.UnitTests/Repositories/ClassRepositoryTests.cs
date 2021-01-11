d43bc33f134ce4866a8a61bf487ec705d837ab94
Wouter Fennis <WouterF@infosupport.com>
2021-01-11 08:08:51 +0100

    started with repository tests

---
+++ Cifra.FileSystem.UnitTests/Repositories/ClassRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using Cifra.Application.Models.Class;
+using Cifra.FileSystem.FileSystemInfo;
+using Cifra.FileSystem.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Cifra.FileSystem.UnitTests.Repositories
+{
+    [TestClass]
+    public class TestsRepositoryTests
+    {
+        private Fixture _fixture;
+        private Mock<IFileLocationProvider> _fileLocationProvider;
+        private Mock<IFileInfoWrapperFactory> _fileInfoWrapperFactory;
+        private ClassRepository _sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fixture = new Fixture();
+            _fileLocationProvider = new Mock<IFileLocationProvider>();
+            _fileInfoWrapperFactory = new Mock<IFileInfoWrapperFactory>();
+
+            _sut = new ClassRepository(_fileLocationProvider.Object, _fileInfoWrapperFactory.Object);
+        }
+        [TestMethod]
+        public void CreateAsync_WithValidClass_AddClassToRepository()
+        {
+            // Arrange
+            Class input = _fixture.Create<Class>();
+
+            // Act
+            _sut.CreateAsync(input);
+
+            // Assert
+        }
+    }
+}
