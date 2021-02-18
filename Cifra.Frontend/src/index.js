189e19cbe88ec28fb164a81db06034d111ae8758
Lucien Krijnen <31688719+lucieneu@users.noreply.github.com>
2021-02-18 18:24:29 +0100

    Converted App.js to jsx file and formated index.js

--- Cifra.Frontend/src/index.js
+++ Cifra.Frontend/src/index.js
@@ -1,28 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import './global/theme.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './global/theme.scss';
 // import "service-worker"
-import App from "./containers/App";
-import { StoreProvider } from "easy-peasy";
-import { store } from "./peasy/store";
+import { StoreProvider } from 'easy-peasy';
+import { store } from './peasy/store';
+import App from './containers/App';
 
-const wrapper = document.getElementById("container");
-wrapper ? ReactDOM.render(
-  <StoreProvider store={store}>
-    <App />
-  </StoreProvider>
-  , wrapper) : false;
+const wrapper = document.getElementById('container');
+wrapper
+  ? ReactDOM.render(
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>,
+      wrapper
+    )
+  : false;
 
-
-console.log('serviceWorker??', 'serviceWorker' in navigator)
+console.log('serviceWorker??', 'serviceWorker' in navigator);
 if ('serviceWorker' in navigator) {
-  console.log('serviceWorker')
+  console.log('serviceWorker');
 
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
   });
 }
