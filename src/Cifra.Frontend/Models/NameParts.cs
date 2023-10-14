91e36ff8611d8fef00eb7694a13942700de6f907
wouter_hilversum@hotmail.com <wouter_hilversum@hotmail.com>
2023-10-14 08:33:02 +0200

    More tests

---
+++ src/Cifra.Frontend/Models/NameParts.cs
@@ -0,0 +1,48 @@
+﻿using Cifra.Frontend.Constants;
+
+namespace Cifra.Frontend.Models
+{
+    internal class NameParts
+    {
+        public string FirstName { get; init; }
+        public string? Infix { get; init; }
+        public string LastName { get; init; }
+
+        private NameParts(string firstName, string? infix, string lastName)
+        {
+            FirstName = firstName;
+            Infix = infix;
+            LastName = lastName;
+        }
+
+        public static NameParts Create(string completeName)
+        {
+            string[] parts = completeName.Split(' ');
+            string firstName = parts.First();
+
+            string? infix = null;
+            string lastName = completeName
+                    .Replace(firstName, string.Empty)
+                    .TrimStart();
+            foreach (var part in parts.Skip(1))
+            {
+                if (DutchInfixes.AllInfixes.Contains(part))
+                {
+                    infix += $" {part}";
+                }
+            }
+
+            if (infix == null)
+            {
+                return new NameParts(firstName, infix, lastName);
+            }
+
+            infix = infix.TrimStart();
+            lastName = lastName
+                .Replace(infix, string.Empty)
+                .TrimStart();
+
+            return new NameParts(firstName, infix, lastName);
+        }
+    }
+}
---
+++ test/Cifra.Frontend.Tests/Models/NamePartsTests.cs
@@ -0,0 +1,51 @@
+﻿using Cifra.Frontend.Models;
+using FluentAssertions;
+
+namespace Cifra.Frontend.Tests.Extensions
+{
+    [TestClass]
+    public class NamePartsTests
+    {
+        [TestMethod]
+        [DataRow("Luuk Berg", "Luuk", "Berg")]
+        [DataRow("Perez y Lucientes", "Perez", "y Lucientes")] 
+        public void Create_WithNoDutchInfix_ReturnsFirstnameAndLastnameSeparate(string completeName, string expectedFirstName, string expectedLastName)
+        {
+            // Act
+            var result = NameParts.Create(completeName);
+
+            // Assert
+            result.FirstName.Should().Be(expectedFirstName);
+            result.Infix.Should().BeNull();
+            result.LastName.Should().Be(expectedLastName);
+        }
+
+        [TestMethod]
+        [DataRow("Luuk van Berg", "Luuk", "van", "Berg")]
+        [DataRow("Van van Berg", "Van", "van", "Berg")]
+        public void Create_WithOneWordDutchInfix_ReturnsFirstnameInfixAndLastnameSeparate(string completeLastName, string expectedFirstName, string expectedInfix, string expectedLastName)
+        {
+            // Act
+            var result = NameParts.Create(completeLastName);
+
+            // Assert
+            result.FirstName.Should().Be(expectedFirstName);
+            result.Infix.Should().Be(expectedInfix);
+            result.LastName.Should().Be(expectedLastName);
+        }
+
+        [TestMethod]
+        [DataRow("Luuk van der Berg", "Luuk", "van der", "Berg")]
+        [DataRow("Louise De La Fontaine", "Louise", "De La", "Fontaine")]
+        public void Create_WithTwoWordsDutchInfix_ReturnsInfixAndLastnameSeparate(string completeLastName, string expectedFirstName, string expectedInfix, string expectedLastName)
+        {
+            // Act
+            var result = NameParts.Create(completeLastName);
+
+            // Assert
+            result.FirstName.Should().Be(expectedFirstName);
+            result.Infix.Should().Be(expectedInfix);
+            result.LastName.Should().Be(expectedLastName);
+        }
+    }
+}
